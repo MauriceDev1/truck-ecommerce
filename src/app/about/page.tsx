df2ce9ab10db7d553a92e3fbf02d73e7b3a8a8fd
MauriceDev1 <mrvolkyn@gmail.com>
2024-10-08 13:14:04 +0200

    fix eslint

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,10 +7,10 @@ const About = () => {
             <div className="w-full py-16 flex gap-8">
                 <div className="w-2/3">
                     <p className="text-lg">
-                    At [Your Business Name], we understand the high cost of maintaining a fleet of trucks firsthand. While managing a logistics company and running a fleet, we saw the constant demand for quality truck parts. Too often, businesses face the difficult choice between expensive new parts or unreliable, low-grade aftermarket options. That's why we created [Your Business Name] to offer a better, more affordable solution.
-                    <br />
-                    <br />
-                    Our mission is simple: provide top-quality, refurbished truck parts that deliver the performance of new parts without the hefty price tag. Every part in our inventory is thoroughly tested, inspected, and restored to meet industry standards, ensuring you get reliability and value. Whether you're managing a fleet or a single vehicle, we help you keep your trucks on the road without breaking the bank.
+                        At Mo Parts, we understand the high cost of maintaining a fleet of trucks firsthand. While managing a logistics company and running a fleet, we saw the constant demand for quality truck parts. Too often, businesses face the difficult choice between expensive new parts or unreliable, low-grade aftermarket options. That&#39;s why we created Mo Parts to offer a better, more affordable solution.
+                        <br />
+                        <br />
+                        Our mission is simple: provide top-quality, refurbished truck parts that deliver the performance of new parts without the hefty price tag. Every part in our inventory is thoroughly tested, inspected, and restored to meet industry standards, ensuring you get reliability and value. Whether you&#39;re managing a fleet or a single vehicle, we help you keep your trucks on the road without breaking the bank.
                     </p>
                 </div>
                 <div className="w-1/3 bg-red-500 h-80" role="img" aria-label="Decorative red background"></div>
